3dc08174774d0e9e2f01d0deb68fccdbeb5e88ab
sNishant011 <shresthanishant011@gmail.com>
2022-03-02 22:08:37 +0545

    completed

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var lightMode = true
 const toggleTheme = () => {
   if (lightMode) {
     document.body.classList.add('dark')
-    document.getElementById('themeToggleBtn').innerText = 'Light'
+    document.getElementById('themeToggleBtn').classList.add('dark')
+    document.getElementById('themeToggleBtn').classList.remove('light')
   } else {
     document.body.classList.remove('dark')
-    document.getElementById('themeToggleBtn').innerText = 'Dark'
+    document.getElementById('themeToggleBtn').classList.remove('dark')
+    document.getElementById('themeToggleBtn').classList.add('light')
   }
   lightMode = !lightMode
 }
@@ -16,4 +18,18 @@ const focusElement = (elementID) => {
   console.log(elementID)
   document.getElementById(elementID).classList.add('focus')
   setTimeout(() => removeElementFocus(elementID), 2000)
+}
+window.onscroll = () => {
+  navFunction()
+}
+
+var prevScrollpos = window.pageYOffset
+window.onscroll = () => {
+  var currentScrollPos = window.pageYOffset
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('header').style.top = '0'
+  } else {
+    document.getElementById('header').style.top = '-70px'
+  }
+  prevScrollpos = currentScrollPos
 }

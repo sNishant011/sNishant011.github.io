8aa8c4eb2e36b390a9fc106a16f6010713cbc019
sNishant011 <shresthanishant011@gmail.com>
2022-05-17 17:54:31 +0545

    added theme local storage, preloader

--- main.js
+++ main.js
@@ -10,10 +10,12 @@ const toggleTheme = () => {
     document.getElementById('themeToggleBtn').classList.add('light')
   }
   lightMode = !lightMode
+  localStorage.setItem('theme', lightMode ? 'light' : 'dark')
 }
 const removeElementFocus = (elementID) => {
   document.getElementById(elementID).classList.remove('focus')
 }
+
 const focusElement = (elementID) => {
   console.log(elementID)
   document.getElementById(elementID).classList.add('focus')
@@ -21,6 +23,21 @@ const focusElement = (elementID) => {
 }
 window.onscroll = () => {
   navFunction()
+}
+window.onload = () => {
+  const prevTheme = localStorage.getItem('theme')
+
+  if (prevTheme === 'dark') {
+    toggleTheme()
+  }
+
+  setTimeout(() => {
+    document.getElementById('preloader').style.opacity = 0;
+    document.getElementById('preloader').style.transform = "scale(3)";
+    document.getElementById('preloader').style.zIndex = -5;
+    document.getElementById('app').style.opacity = 1;
+
+  }, 2000)
 }
 
 var prevScrollpos = window.pageYOffset
